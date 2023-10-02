3de736aa372283af995b605ba949b99c46aff1f1
Javier <moncadajavier2@yahoo.com>
2023-10-02 17:52:50 -0400

    init commit

---
+++ src/main/java/net/chamber/InTheChamberOfMammonDataGenerator.java
@@ -0,0 +1,11 @@
+package net.chamber;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class InTheChamberOfMammonDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
---
+++ src/main/java/net/chamber/IntheChamberofMammonClient.java
@@ -0,0 +1,10 @@
+package net.chamber;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class IntheChamberofMammonClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
